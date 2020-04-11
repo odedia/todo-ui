7e0bf9c3f4fa0c6d2ee2bf9f7aa119405f5df829
Oded Shopen <odedia@gmail.com>
2020-04-11 23:09:07 +0300

    first commit

---
+++ src/Api.js
@@ -0,0 +1,24 @@
+import axios from 'axios'  
+
+//The updated code is using PCF route mappings instead of port binding.
+//const SERVER_URL = 'http://localhost:9000';  
+  
+const instance = axios.create({  
+  baseURL: 'http://' + window.location.hostname + '/api',
+  timeout: 1000  
+});  
+  
+export default {  
+  // (C)reate  
+  createNew: (text, completed) => instance.post('todos', {title: text, completed: completed}),  
+  // (R)ead  
+  getAll: () => instance.get('todos', {  
+    transformResponse: [function (data) {  
+      return data ? JSON.parse(data)._embedded.todos : data;  
+    }]  
+  }),  
+  // (U)pdate API
+  updateForId: (id, text, completed) => instance.put('todos/'+id, {title: text, completed: completed}),  
+  // (D)elete API
+  removeForId: (id) => instance.delete('todos/'+id)  
+}
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Todos />
+    <footer class="info">
+      <p>This demo is based on a project written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
+      <p>First modified for this tutorial by Andrew Hughes</p>
+      <p>PCF modifications for this tutorial were made by Oded Shopen</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  import Todos from './components/Todos'
+  // app Vue instance
+  const app = {
+    name: 'app',
+    components: {
+      Todos
+    },
+    // app initial state
+    data: () => {
+      return {
+      }
+    }
+  }
+
+  export default app
+</script>
+
+<style>
+  [v-cloak] { display: none; }
+</style>
